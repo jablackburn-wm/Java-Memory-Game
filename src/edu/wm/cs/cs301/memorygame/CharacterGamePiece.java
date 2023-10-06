f4ba54d2b959d6e6dab0b06fa62362b25ee29e06
Jake Blackburn <jakeblackburn@Johns-MacBook-Air.local>
2023-10-06 13:08:17 -0400

    Implement CharacterGamePiece

--- src/edu/wm/cs/cs301/memorygame/CharacterGamePiece.java
+++ src/edu/wm/cs/cs301/memorygame/CharacterGamePiece.java
@@ -2,25 +2,26 @@ package edu.wm.cs.cs301.memorygame;
 
 public class CharacterGamePiece implements GamePiece {
 	private final Character symbol;
+	private boolean is_visible = false;
 	
 	public CharacterGamePiece(char s) {
 		this.symbol = s;
 	}
 
 	public Character getSymbol() {
-
+		return symbol;
 	}
 	
 	public void setVisible(boolean v) {
-
+		this.is_visible = v;
 	}
 	
 	public boolean isVisible() {
-
+		return this.is_visible;
 	}
 	
 	public boolean equals(GamePiece other) {
-
+		return (other.getSymbol().equals(this.symbol));
 	}
 	
 }
