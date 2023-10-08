8148e68852757ec1742600f2a700118401363c9b
Jake Blackburn <jakeblackburn@Johns-MacBook-Air.local>
2023-10-08 12:59:59 -0400

    Add randomizeChars

--- src/edu/wm/cs/cs301/memorygame/GameBoard.java
+++ src/edu/wm/cs/cs301/memorygame/GameBoard.java
@@ -1,9 +1,53 @@
 package edu.wm.cs.cs301.memorygame;
 
+import java.util.Collections;
+
 public class GameBoard {
 	private final GamePiece[][] board;
+	private final int rows;
+	private final int cols;
 	
 	public GameBoard(int rows, int cols, Alphabet a) {
+		this.rows = rows;
+		this.cols = cols;
 
+		int num_chars = (rows * cols) / 2;
+
+		char[] chars = copyOfRange(a.toCharArray(), 0, num_chars);
+		char[] randomized_chars = randomizeChars(chars);
+
+		board = populateBoard(rows, columns, randomized_chars);
+	}
+
+	public void drawBoard() {
+		
+	}
+	
+	private
+	
+	public GamePiece makeGuess(int row, int col) {
+		GamePiece guess = board[row][col];
+		if guess.isVisible() == true
+		guess.setVisible(true)
+	}
+
+	
+
+
+	private GamePiece[][] populateBoard(int rows, int columns, char[] randomized_chars) {
+		
+	}
+
+  private char[] randomizeChars(char[] chars) {
+		int chars_len = chars.length;
+		char[] doubled_chars = new char[chars_len * 2];
+		int i = 0;
+		for (char c : chars) {
+			doubled_chars[i] = c;
+			doubled_chars[i + 1] = c;
+			i = i + 2;
+		}
+		Collections.shuffle(doubled_chars);
+		return doubled_chars;
 	}
 }
